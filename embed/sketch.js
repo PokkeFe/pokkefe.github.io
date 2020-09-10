b32b34c95da6f81e6ef4de411e775867c90e9c9c
Cole Potter <pott6366@stthomas.edu>
2020-09-10 14:52:43 -0500

    Added embed page

---
+++ embed/sketch.js
@@ -0,0 +1,41 @@
+const WIDTH = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+const HEIGHT = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+let myBox;
+
+function setup() {
+    createCanvas(WIDTH, HEIGHT);
+    rectMode(CENTER);
+    myBox = new Box(width/2, height/2);
+}
+
+function draw() {
+    background(255);
+    myBox.update();
+    myBox.draw();
+}
+
+class Box {
+    constructor(x, y) {
+        this.pos = createVector(x, y);
+        this.rot = 0;
+        this.rotSpeed = 0.03;
+    }
+
+    update() {
+        this.rot += this.rotSpeed;
+    }
+
+    draw() {
+        push();
+        translate(this.pos.x, this.pos.y);
+        rotate(this.rot);
+        noStroke();
+        fill(100);
+        ellipse(0, 0, 40, 40);
+        fill(255);
+        ellipse(0, 0, 25, 25);
+        rect(0, 0, 10, 40);
+        pop();
+    }
+}
